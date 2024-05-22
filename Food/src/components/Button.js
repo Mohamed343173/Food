3b6456ea624f10dc3620a5323310c589bf3bd3a1
mohamed343173 <mohamedridha580@gmail.com>
2024-05-22 13:30:22 +0100

    first commit

--- Food/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import { Link } from "react-router-dom";
-
-const LinkButton = ({ className, to, onClick, children }) => {
-    return <Link onClick={onClick} className={className} to={to}>{children}</Link>;
-}
-
-export default LinkButton;
